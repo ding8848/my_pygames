3ee2a0a834d59e7b2f3c5b3606ec2f7e19b2d18b
Black Bob <china.bob.ding@gmail.com>
2021-05-08 20:39:20 +0800

    Prepare for crazyforhoney game.

---
+++ src/crazyforhoney.py
@@ -0,0 +1,10 @@
+import sys, pygame
+
+pygame.init()
+size = weight, height = 600, 800
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Crazy for honey")
+
+while 1:ßß
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
